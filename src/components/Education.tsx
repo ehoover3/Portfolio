b5caa6b447bdfb03ecf4266fd1d8d9c6dd289440
Eric Hoover <ehoover3@yahoo.com>
2023-07-26 01:42:59 -0400

    updated resume

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import EducationBox from "./EducationBox";
 import IndianaUniversity from "../assets/IndianaUniversity.png";
-import PurdueUniversitiy from "../assets/PurdueUniversitiy.png";
+import PurdueUniversity from "../assets/PurdueUniversitiy.png";
 import Heading from "./heading";
 
 function Education() {
@@ -14,7 +14,7 @@ function Education() {
         </div>
         <Row className='justify-content-center'>
           <EducationBox img={IndianaUniversity} title='MBA, Finance Concentration' description='Indiana University • 3.95/4.0 GPA' />
-          <EducationBox img={PurdueUniversitiy} title='Bachelor of Communication' description='Purdue University • 3.42/4.0 GPA' />
+          <EducationBox img={PurdueUniversity} title='Bachelor of Communication' description='Purdue University • 3.42/4.0 GPA' />
         </Row>
       </Container>
     </div>
