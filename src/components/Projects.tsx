383412313dd11d5527e560b7e663c60b4f411671
Eric Hoover <ehoover3@yahoo.com>
2023-09-07 22:38:24 -0400

    added dose schedule

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,9 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import ProjectsBox from "./ProjectsBox";
-import img1 from "../assets/img-6.png";
-import img2 from "../assets/img-7.png";
-import img3 from "../assets/img-8.png";
 import Heading from "./heading";
 import img1svg from "../assets/icon-1.svg";
+import img2svg from "../assets/icon-2.svg";
 
 function Projects() {
   return (
@@ -16,7 +14,7 @@ function Projects() {
         </div>
         <Row className='justify-content-center'>
           <ProjectsBox img={img1svg} href={"https://www.careerplantoday.com/"} title='Career Plan Today' />
-          {/* <ProjectsBox img={img2} title='Therapy AI' cost='Cost $12,783' /> */}
+          <ProjectsBox img={img2svg} href={"https://www.doseschedule.com/"} title='Dose Schedule' />
           {/* <ProjectsBox img={img3} title='University of Notre Dame' cost='Cost $12,783' /> */}
         </Row>
       </Container>
