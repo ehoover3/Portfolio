60e5743f334a5a32454d8973bd61448efa4eebc5
Eric Hoover <ehoover3@yahoo.com>
2024-04-11 01:56:23 -0400

    add aws security specialty

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,22 +20,17 @@ function About() {
             <div className='mb-3'>
               <Heading title='About Me' />
             </div>
-            <p className='sub-heading'>
-              I’m a full-stack software engineer with a passion for empathy and a love for understanding how businesses can deliver greater value to
-              the people they serve.
-            </p>
+            <p className='sub-heading'>I’m a full-stack software engineer with a passion for empathy and a love for understanding how businesses can deliver greater value to the people they serve.</p>
             <ListGroup>
               <ListGroup.Item className='d-flex align-items-center'>
                 <div className='check'>
                   <svg width='21' height='12' viewBox='0 0 31 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M9.91273 17.2739L2.87296 10.4609L0.526367 12.7319L9.91273 21.8159L30.0264 2.35012L27.6798 0.0791016L9.91273 17.2739Z'
-                      fill='black'
-                    />
+                    <path d='M9.91273 17.2739L2.87296 10.4609L0.526367 12.7319L9.91273 21.8159L30.0264 2.35012L27.6798 0.0791016L9.91273 17.2739Z' fill='black' />
                   </svg>
                 </div>
                 <p>
                   Certifications incude:
+                  <li>AWS Security Specialty</li>
                   <li>AWS SysOps Administrator Associate</li>
                   <li>AWS Solutions Architect Associate</li>
                   <li>AWS Developer Associate</li>
