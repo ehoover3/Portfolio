1148c3d5839e5a61aa4d189a51fc78fcdeb4c54d
Eric Hoover <ehoover3@yahoo.com>
2024-10-16 09:55:46 -0400

    suspend projects component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <About />
         <Experience />
         <Certifications />
-        <Projects />
+        {/* <Projects /> */}
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
